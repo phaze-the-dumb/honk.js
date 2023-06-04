38f9efb5459e61e6ded0ff8be579be32cb860976
phaze-the-dumb <phaze@duck.com>
2023-06-04 13:19:34 +0100

    fix more broken parser shit

--- honk.js
+++ honk.js
@@ -53,15 +53,62 @@ class HONK{
                     console.log(index, indents, idnts, l)
 
                 if(idnts == 0)return;
-                for (let i = 0; i < idnts; i++)
+                for (let i = 0; i < idnts; i++){
                     currentParent = currentParent.parent;
+                }
             }
 
             if(indents > 0){
                 if(!currentParent)
                     throw new Error("Object/Array doesn't exist, Line "+l.trim())
 
-                if(splitLine[1]){
+                if(Array.isArray(currentParent)) {
+                    if(splitLine[0].trim().endsWith(':')){
+                        // Check if vaild array
+                        let nxtLine = lines[index + 1];
+                        let indents2 = 0;
+                        let inWord2 = false;
+
+                        nxtLine.split('').forEach(char => {
+                            if(char === ' ' && !inWord2)
+                                indents2 += 0.25;
+                            else 
+                                inWord2 = true;
+                        });
+
+                        indents2 = Math.floor(indents2);
+                        lastIndent = indents2;
+
+                        if(indents2 == 0)
+                            throw new Error("Not a vaild object/array: "+splitLine[0])
+
+                        if(nxtLine.includes(': '))
+                            currentParent[splitLine[0].trim()] = {
+                                parent: currentParent
+                            };
+                        else {
+                            let tmpParent = currentParent;
+
+                            currentParent[splitLine[0].trim()] = [];
+                            currentParent[splitLine[0].trim()].parent = tmpParent;
+                        }
+
+                        if(this.debug)
+                            console.log('Original:', l, 'Parsed:', splitLine);
+
+                        currentParent = currentParent[splitLine[0].trim()];
+                    } else{
+                        if(splitLine[1]){
+                            let key = splitLine[0].trim();
+
+                            splitLine.shift();
+                            splitLine = splitLine.join(': ');
+
+                            currentParent[key] = splitLine;
+                        } else
+                            currentParent.push(splitLine[0].trim());
+                    }
+                } else if(splitLine[1]){
                     let key = splitLine[0].trim();
 
                     splitLine.shift();
